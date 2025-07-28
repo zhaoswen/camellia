8c8e0615fd691d72bb49229432498d60270cf9d2
noah <hello@tineaine.cn>
2025-07-28 18:01:42 +0800

    refactor(divider): 优化分割线组件文档注释
    - 为 Divider 类添加详细的文档注释，说明各个属性的作用和默认值
    - 为构造函数参数添加文档注释
    - 优化代码结构，提高可读性

--- lib/widgets/display/divider.dart
+++ lib/widgets/display/divider.dart
@@ -2,7 +2,27 @@ import 'package:flutter/material.dart';
 
 import '../../../entity/common.dart';
 
+/// 分割线组件
+///
+/// 用于在界面中添加分割线
+///
+/// [direction] 分割线方向，默认为水平
+///
+/// [width] 分割线宽度，默认为1
+///
+/// [height] 分割线高度，默认为1
+///
+/// [color] 分割线颜色，默认为Colors.black12
+///
+/// 示例：Divider(direction: Direction.horizontal, color: Colors.red)
 class Divider extends StatelessWidget {
+  /// 分割线方向，默认为水平
+  ///
+  /// 分割线宽度，默认为1
+  ///
+  /// 分割线高度，默认为1
+  ///
+  /// 分割线颜色，默认为Colors.black12
   const Divider({
     super.key,
     required this.direction,
@@ -11,16 +31,16 @@ class Divider extends StatelessWidget {
     this.color = Colors.black12,
   });
 
-  // 分割线方向
+  /// 分割线方向
   final Direction direction;
 
-  // 分割线宽度
+  /// 分割线宽度
   final double width;
 
-  // 分割线高度
+  /// 分割线高度
   final double height;
 
-  // 分割线颜色
+  /// 分割线颜色
   final Color color;
 
   @override
